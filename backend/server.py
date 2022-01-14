d681c6a3fffe29b9dec9c8cfbeb625687e5476b5
Roland <roland.burke@gmx.de>
2022-01-14 11:20:12 +0100

    refactoring

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+# general
 import psutil
 import time
+
+# webserver
+from flask import Flask, render_template
 import socket
-import adafruit_dht
-import board
-import time
-import curses
+
+# sensors
+#import adafruit_dht
+#import board
+
 
 # init sensor
-dht_device = adafruit_dht.DHT11(board.D4)
+#dht_device = adafruit_dht.DHT11(board.D4)
 
 app = Flask(__name__)
 
@@ -17,8 +21,13 @@ def getUptime():
     return time.strftime('%H:%M:%S', time.gmtime(uptime))
 
 def getSensorData():
-	temperature = dht_device.temperature
-	humidity = dht_device.humidity
+	try:
+		temperature = dht_device.temperature
+		humidity = dht_device.humidity
+	except:
+		temperature = -1
+		humidity = -1
+     
 	data = {
 		'temperature' : temperature,
 		'humidity': humidity
@@ -30,13 +39,16 @@ def getData():
          'deviceName' : socket.gethostname(),
          'temperature': getSensorData().get('temperature'),
          'humidity': getSensorData().get('humidity'),
+         'pressure': -1,
+         'co2': -1,
+         'tvoc': -1,
          'uptime': getUptime()
     }
     return data
 
 @app.route('/')
 def welcome():
-	return render_template('index.html', **getData())
+	return render_template('index.html')
 
 @app.route('/api/data')
 def data():
