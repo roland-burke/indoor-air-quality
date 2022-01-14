eb21499d0799767b558409b5f019062f4dc9edbc
Roland <roland.burke@gmx.de>
2022-01-14 11:19:47 +0100

    added vue application

---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="board">
+		<h1>{{ msg }}</h1>
+		<p>Device Name {{ deviceName }}</p>
+		<div class="text-xl text-left text-red-700">Temperature: {{ temperature }}°C</div>
+		<div>Humidity: {{ humidity }}%</div>
+		<div>Pressure: {{ pressure }}hPa</div>
+		<div>TVOC: {{ tvoc }}°C</div>
+		<div>CO2: {{ co2 }}ppm</div>
+		<div>Uptime: {{ uptime }}</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'Dashboard',
+	props: {
+		msg: String
+	},
+	data: function () {
+		return {
+			deviceName: 'dummyDevice',
+			uptime: '00:02:32',
+			temperature: 12.3,
+			humidtiy: 34,
+			pressure: 1200,
+			co2: 800,
+			tvoc: 100
+		}
+	}
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.board {
+	margin: 40px 0 0;
+	border: 4px solid #4242b9;
+	border-radius: 6px;
+}
+
+.temperature {
+	font-size: 2rem;
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home">
+		<Dashboard msg="Welcome to Your Vue.js + TypeScript App" />
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Dashboard from '@/components/Dashboard.vue' // @ is an alias to /src
+
+export default defineComponent({
+	name: 'Home',
+	components: {
+		Dashboard
+	}
+})
+</script>
