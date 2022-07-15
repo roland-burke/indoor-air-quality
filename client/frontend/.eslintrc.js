472eab2da19fba7e37b6dc3cecd34fa2d296607b
Roland <roland.burke@gmx.de>
2022-07-15 13:55:13 +0200

    added warning and alert messages

--- client/frontend/.eslintrc.js
+++ client/frontend/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-tabs': 0,
-		indent: [2, 'tab'],
+		indent: [2, 'tab', { "SwitchCase": 1 }],
 		'space-before-function-paren': ['error', {
 			anonymous: 'never',
 			named: 'never',
--- client/frontend/src/components/QualityIndex.vue
+++ client/frontend/src/components/QualityIndex.vue
@@ -7,7 +7,9 @@
 		<div v-if="this.indexLevel > 4" class="bg-red-500 w-1/6 h-5"></div>
 		<div v-if="this.indexLevel > 5" class="bg-red-700 w-1/6 h-5"></div>
 	</div>
-	<div>Level: {{ indexLevel }} ({{this.getLevelName(this.indexLevel)}})</div>
+	<div>Level: {{ indexLevel }} ({{ this.getLevelName(this.indexLevel) }})</div>
+	<div v-if="this.indexLevel === 4 || this.indexLevel === 5" class="p-4 mt-3 mb-2 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800" role="alert"><span class="font-bold">Warning!</span> Air Quality is in a critical condition, please take action!</div>
+	<div v-if="this.indexLevel === 6" class="p-4 mt-3 mb-2 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert"><span class="font-bold">Danger!</span> Air Quality is in a serious condition! Please take immediate Action!</div>
 </template>
 
 <script lang='ts'>
@@ -22,20 +24,20 @@ export default defineComponent({
 	methods: {
 		getLevelName: function(level: number): string {
 			switch (level) {
-			case 1:
-				return 'Excellent'
-			case 2:
-				return 'Fine'
-			case 3:
-				return 'Moderate'
-			case 4:
-				return 'Poor'
-			case 5:
-				return 'Very Poor'
-			case 6:
-				return 'Dangerous'
-			default:
-				return 'Undefined'
+				case 1:
+					return 'Excellent'
+				case 2:
+					return 'Fine'
+				case 3:
+					return 'Moderate'
+				case 4:
+					return 'Poor'
+				case 5:
+					return 'Very Poor'
+				case 6:
+					return 'Dangerous'
+				default:
+					return 'Undefined'
 			}
 		}
 	}
